de1da066ec3e344a2db77605e87170820c4bf342
ali <a_icam@yahoo.com>
2022-10-10 15:38:06 -0700

    admin endpoints

--- database/admin/models.py
+++ database/admin/models.py
@@ -11,3 +11,18 @@ class AdminUser(Base):
     email = Column(String(30), unique=True, index=True)
     hashed_password = Column(String(50))
     name = Column(String(20))
+
+class University(Base):
+    __tablename__ = "university"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(30), unique=True, index=True)
+
+class UniversityLabel(Base):
+    __tablename__ = "university_scores"
+
+    id = Column(Integer, primary_key=True, index=True)
+    university_id = Column(Integer)
+    label_name = Column(String(50))
+    label_min = Column(String(50))
+    label_mid = Column(String(50))
+    label_max = Column(String(50))
