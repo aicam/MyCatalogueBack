3b7136364760403add46b13879d45ea054bc4e8a
TheLDM <34075776+TheLDM@users.noreply.github.com>
2022-10-10 18:53:38 -0700

    Update crud.py
    - updated to user model I was using

--- database/admin/crud.py
+++ database/admin/crud.py
@@ -5,19 +5,21 @@ from . import schemas
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(models.AdminUser).filter(models.AdminUser.id == user_id).first()
+    return db.query(models.SystemUser).filter(models.SystemUser.id == user_id).first()
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(models.AdminUser).filter(models.AdminUser.email == email).first()
-
+    return db.query(models.SystemUser).filter(models.SystemUser.email == email).first()
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.AdminUser).offset(skip).limit(limit).all()
+    return db.query(models.SystemUser).offset(skip).limit(limit).all()
+
+def get_univ(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.UnivInfo).offset(skip).limit(limit).all()
 
 
 def create_user(db: Session, user: schemas.AdminCredentials):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.AdminUser(email=user.email, hashed_password=fake_hashed_password)
+    fake_hashed_password = user.password + 'notreallyhashed'
+    db_user = models.SystemUser(email=user.email, hashed_password=fake_hashed_password, role=user.role)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
