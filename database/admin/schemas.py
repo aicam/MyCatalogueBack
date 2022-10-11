a414de598f34dfed380d899b041bfd466ce89e32
TheLDM <34075776+TheLDM@users.noreply.github.com>
2022-10-10 18:56:22 -0700

    Update schemas.py
    - edited to user base model
    - added role field as an enum
    - added university

--- database/admin/schemas.py
+++ database/admin/schemas.py
@@ -1,10 +1,36 @@
 from pydantic import BaseModel
+from enum import Enum
 
-class AdminBase(BaseModel):
+class RoleEnum(str, Enum):
+    student = 'student'
+    univ = 'univ'
+    admin = 'admin'
+
+
+class UserBase(BaseModel):
     email: str
-class AdminCredentials(AdminBase):
+class AdminCredentials(UserBase):
     password: str
-class Admin(AdminBase):
+    role: RoleEnum = RoleEnum.admin
+class StudentCredentials(UserBase):
+    password: str
+    role: RoleEnum = RoleEnum.student
+class UnivCredentials(UserBase):
+    password: str
+    role: RoleEnum = RoleEnum.univ
+class User(UserBase):
     id: int
+    class Config:
+        orm_mode = True
+
+class UnivBase(BaseModel):
+    uni_name: str
+    min_sat: int
+    min_act: int
+    capacity: int
+    accept_rate: float
+
+class Univ(UnivBase):
+    uni_id: int
     class Config:
         orm_mode = True
