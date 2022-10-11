321ad1a425a45f843d3e95d678673ddd68511001
TheLDM <34075776+TheLDM@users.noreply.github.com>
2022-10-10 18:51:16 -0700

    Add files via upload

---
+++ router/university.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+import sys
+sys.path.append('../parentdirectory')
+from database.admin import schemas, crud
+from dependencies import get_db, generate_key
+
+router = APIRouter(
+    prefix="/univ"
+)
+
+# @router.get("/users/", response_model=List[schemas.User])
+# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     db_user = crud.get_users(db, skip=skip, limit=limit)
+#     return db_user
+
+@router.post("/login/")
+def login(user: schemas.UnivCredentials, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if not db_user:
+        raise HTTPException(status_code=403, detail="Wrong username or password")
+    if db_user.hashed_password != user.password + "notreallyhashed":
+        raise HTTPException(status_code=403, detail="Wrong username or password")
+    return {'key': generate_key(user.email)}
