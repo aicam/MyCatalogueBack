b1a782f1855b8237a275e21c07bf7f052eda16e1
TheLDM <34075776+TheLDM@users.noreply.github.com>
2022-10-10 19:06:26 -0700

    Update models.py

--- database/admin/models.py
+++ database/admin/models.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import relationship
 from .funcs import Base
 
 
-class AdminUser(Base):
-    __tablename__ = "admin_users"
+class SystemUser(Base):
+    __tablename__ = "system_users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(30), unique=True, index=True)
     hashed_password = Column(String(50))
-    name = Column(String(20))
+    role = Column(String(10))
 
 class UnivInfo(Base):
     __tablename__ = "univ_info"
