fd67d3ceb037a483657fedf6a6f4b3c27d8d9adf
TheLDM <34075776+TheLDM@users.noreply.github.com>
2022-10-12 15:10:06 -0700

    Update crud.py
    - added def to get univ by id
    - added def to update univ data from db

--- database/admin/crud.py
+++ database/admin/crud.py
@@ -16,6 +16,19 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 def get_univ(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.UnivInfo).offset(skip).limit(limit).all()
 
+def get_univ_by_id(db: Session, uni_id: int):
+    return db.query(models.UnivInfo).filter(models.UnivInfo.uni_id == uni_id).first()
+
+def update_univ_info(db: Session, univ: schemas.UnivEdit, uni_id:int):
+    db_uni = get_univ_by_id(db, uni_id)
+    univ_data = univ.dict(exclude_unset=True)
+    for key,value in univ_data.items():
+        setattr(db_uni, key, value)
+    db.add(db_uni)
+    db.commit()
+    db.refresh(db_uni)
+    return db_uni
+
 
 def create_user(db: Session, user: schemas.AdminCredentials):
     fake_hashed_password = user.password + 'notreallyhashed'
