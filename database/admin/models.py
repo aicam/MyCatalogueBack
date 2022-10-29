3ea898ecab4b6ad6e989f3730fc7b16b224c4a7c
TheLDM <34075776+TheLDM@users.noreply.github.com>
2022-10-29 14:37:34 -0700

    Update models.py
    import date

--- database/admin/models.py
+++ database/admin/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DECIMAL
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DECIMAL, Date
 from sqlalchemy.orm import relationship
 
 from .funcs import Base
