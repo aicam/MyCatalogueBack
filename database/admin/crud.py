b363665a193151736daf1e5277b9420f6485a4ee
TheLDM <34075776+TheLDM@users.noreply.github.com>
2022-10-29 14:30:47 -0700

    Update crud.py
    - added crud for applications/ test scores

--- database/admin/crud.py
+++ database/admin/crud.py
@@ -58,9 +58,16 @@ def update_univ_info(db: Session, univ: schemas.UnivEdit, uni_id: int):
 def get_profile(db: Session, user_id:int):
     return db.query(models.StudentInfo).filter(models.StudentInfo.user_id == user_id).first()
 
-def get_student_apps(db: Session, user_id: int):
-    return db.query(models.StudentApplications).filter(models.StudentApplications.user_id == user_id)
+def get_student_apps(db: Session, student_id: int):
+    return db.query(models.StudentApplications).filter(models.StudentApplications.student_id == student_id).all()
 
+def get_student_scores(db: Session, student_id: int):
+    return db.query(models.TestScores).filter(models.TestScores.student_id == student_id).all()
+
+def get_student_score(db: Session, score_id: int):
+    return db.query(models.TestScores).filter(models.TestScores.score_id == score_id).first()
+
+# new update methods
 def update_student_info(db: Session, student: schemas.StudentEdit, user_id: int):
     db_student = get_profile(db, user_id)
     student_data = student.dict(exclude_unset=True)
@@ -71,9 +78,27 @@ def update_student_info(db: Session, student: schemas.StudentEdit, user_id: int)
     db.refresh(db_student)
     return db_student
 
+
 def new_application(db: Session, app: schemas.AppBase):
     db_new_app = models.StudentApplications(uni_name = app.uni_name, app_date = app.app_date, student_id = app.student_id)
     db.add(db_new_app)
     db.commit()
     db.refresh(db_new_app)
     return db_new_app
+
+def new_test_score(db: Session, score: schemas.TestBase):
+    db_new_score = models.TestScores(test_name = score.test_name, t_score = score.t_score, student_id = score.student_id)
+    db.add(db_new_score)
+    db.commit()
+    db.refresh(db_new_score)
+    return db_new_score
+
+def update_score_info(db: Session, score: schemas.TestEdit, score_id: int):
+    db_score = get_student_score(db, score_id)
+    score_data = score.dict(exclude_unset=True)
+    for key,value in score_data.items():
+        setattr(db_score, key, value)
+    db.add(db_score)
+    db.commit()
+    db.refresh(db_score)
+    return db_score
